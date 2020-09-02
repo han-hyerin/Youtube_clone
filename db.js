b37127f03788f8564cddddbf3a74646cf09ec0dd
han-hyerin <68931265+han-hyerin@users.noreply.github.com>
2020-09-02 21:30:32 +0900

    error error....and error........

--- db.js
+++ db.js
@@ -1,50 +1,54 @@
 export const videos = [
-    {
-        id:3234,
-        title="Video awesome",
-        description: "This is something I love",
-        views:24,
-        videoFile:"http://archive.org/details/BigBuckBunny_124",
-        creator:{
-            id=121212,
-            name:"Nicolas",
-            email:"nico@las.com"
-        }
+  {
+    id: 3234,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
     },
-    {
-        id:2343242,
-        title="Video haha",
-        description: "This is something I love",
-        views:24,
-        videoFile:"http://archive.org/details/BigBuckBunny_124",
-        creator:{
-            id=121212,
-            name:"Nicolas",
-            email:"nico@las.com"
-        }
+  },
+  {
+    id: 2343242,
+    title: "Video haha",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
     },
-    {
-        id:3425454,
-        title="Video hoho",
-        description: "This is something I love",
-        views:24,
-        videoFile:"http://archive.org/details/BigBuckBunny_124",
-        creator:{
-            id=121212,
-            name:"Nicolas",
-            email:"nico@las.com"
-        }
+  },
+  {
+    id: 3425454,
+    title: "Video hoho",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
     },
-    {
-        id:32438,
-        title="Video hihi",
-        description: "This is something I love",
-        views:24,
-        videoFile:"http://archive.org/details/BigBuckBunny_124",
-        creator:{
-            id=121212,
-            name:"Nicolas",
-            email:"nico@las.com"
-        }
-    }
+  },
+  {
+    id: 32438,
+    title: "Video hihi",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
 ];
