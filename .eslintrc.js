b3cd889a8a9e3533706c0c87235b01cbb40236bf
han-hyerin <68931265+han-hyerin@users.noreply.github.com>
2020-09-15 12:43:54 +0900

    200915

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,7 @@ module.exports = {
   rules: {
     "no-console": "off",
   },
+  env: {
+    browser: true,
+  },
 };
--- models/User.js
+++ models/User.js
@@ -7,6 +7,18 @@ const UserSchema = new mongoose.Schema({
   avatarUrl: String,
   facebookId: Number,
   githubId: Number,
+  creator: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
 });
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
